8debf248ab2db0bac8b4cba0eddae492892d9670
Roman <r.sheverun@gmail.com>
2018-05-02 00:15:42 +0300

    navbar

---
+++ resources/views/layouts/headerImage.blade.php
@@ -0,0 +1,1 @@
+<img src="../public/img/background.png" alt="" class="mw-100">
---
+++ resources/views/layouts/nav.blade.php
@@ -0,0 +1,10 @@
+<ul class="nav justify-content-center">
+	<li class="nav-item"><a href="#" class="nav-link">Головна</a>
+	</li>
+	<li class="nav-item"><a href="#" class="nav-link">Про нас</a>
+	</li>
+	<li class="nav-item"><a href="#" class="nav-link">Ціни</a>
+	</li>
+	<li class="nav-item"><a href="#" class="nav-link">Контакти</a>
+	</li>
+</ul>
